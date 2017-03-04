8d12c743b28ceb5f13063ac58b8b8c820bbe1a8a
sstaub <sstaub@users.noreply.github.com>
2017-03-04 09:44:37 +0100

    Update EthernetUdp3.cpp
    changed 'unsigned char' to 'uint8_t'

--- src/EthernetUdp3.cpp
+++ src/EthernetUdp3.cpp
@@ -163,7 +163,7 @@ int EthernetUDP::read()
   return -1;
 }
 
-int EthernetUDP::read(unsigned char* buffer, size_t len)
+int EthernetUDP::read(uint8_t* buffer, size_t len)
 {
 
   if (_remaining > 0)
